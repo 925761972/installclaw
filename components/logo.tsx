846ec3a3b3190c35631d30012451564e34fef340
InstallClaw <925761972@qq.com>
2026-07-27 14:19:42 +0800

    feat: show operator name in site header

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-export function Logo() {
+export function Logo({ showOperator = false }: { showOperator?: boolean }) {
   return (
     <Link className="brand" href="/" aria-label="净幕首页">
       <span className="brand-mark" aria-hidden="true"><i /><i /><i /></span>
       <span>净幕</span>
+      {showOperator ? <small className="brand-operator">铭锦澜泽</small> : null}
     </Link>
   );
 }
